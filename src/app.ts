b7ce4ac8e75b207ee1eb44996e17489de24996c6
Ben Currie <benjaminrussellcurrie@gmail.com>
2019-07-30 16:08:28 -0500

    Array Spread Operator

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,12 @@
 // 'tsc' in terminal, then 'node dist/app.js' to run in terminal.
 
-// Initial function uses an array of a definite size.
+const toppings = ['bacon', 'chilli'];
 
-// function sumAll(arr) {
-//   return arr.reduce((prev, next) => prev + next);
-// }
+const newToppings = ['pepperoni'];
 
-// const sum = sumAll([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+// Things were previously concatonated. Now the spread operator can handle this for us.
+// This creates a copy with one array and all 3 toppings inside. It does not keep a reference
+// to the original arrays.
+const allToppings = [...toppings, ...newToppings];
 
-// Using rest parameters, we can pass in an indefinite size object.
-// Rest parameters can be used as a secondary argument. For example if we expected a message first.
-function sumAll(message, ...arr) {
-  console.log(message);
-  return arr.reduce((prev, next) => prev + next);
-}
-
-const sum = sumAll('Hello!', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-
-console.log(sum);
+console.log(allToppings);
