b7d19137a147311e81c5ca503da81b86def6b645
Ben Currie <benjaminrussellcurrie@gmail.com>
2019-07-30 16:34:06 -0500

    Destructuring Arrays and Objects

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,57 @@
 // 'tsc' in terminal, then 'node dist/app.js' to run in terminal.
 
-const toppings = ['bacon', 'chilli'];
+const pizza = {
+  name: 'Pepperoni',
+  toppings: ['pepperoni']
+};
 
-const newToppings = ['pepperoni'];
+// We can create a function that expects pizza as an argument.
+// function order(pizza) {
+// };
 
-// Things were previously concatonated. Now the spread operator can handle this for us.
-// This creates a copy with one array and all 3 toppings inside. It does not keep a reference
-// to the original arrays.
-const allToppings = [...toppings, ...newToppings];
+// Now we can call the function and pass in the pizza constant.
+// order(pizza);
 
-console.log(allToppings);
+// You can create an object literal structure around the argument to destructure the const.
+// This means the argument coming in is an object and it has a name property and a toppings property.
+// function order({ name, toppings }) {
+//   console.log(name, toppings);
+// }
+
+// If you needed to rename a property, for example if it needed to be changed before
+// sending it to a server.
+// function order({ name: pizzaName, toppings: pizzaToppings }) {
+//   console.log(pizzaName, pizzaToppings);
+// }
+
+// If we wanted to return a new object with those new properties.
+function order({ name: pizzaName, toppings: pizzaToppings }) {
+  return { pizzaName, pizzaToppings };
+}
+// This would return the entire object with those two properties.
+const myOrder = order(pizza);
+// You can also destructure the returned result if you only want one of the properties.
+// This syntax is very similar to an import statement.
+// Instead of asking for a property from a module, we are asking for a property of an object.
+const { pizzaName } = order(pizza);
+
+// Arrays can be destructured too.
+const toppings = ['bacon', 'pepperoni', 'chilli'];
+
+// Typically you would do this.
+// const firstItem = toppings[0];
+
+// However we can use array destructuring. The names do not matter, only the order matters.
+const [first, second, third] = toppings;
+
+// These will be returned as string values because they are no longer in an array.
+console.log(first, third);
+
+// Arrays can be destructed after they are passed into a function.
+// A typing of : any has been added to the array because of Typescript type checking.
+function logToppings([first, second, third]: any) {
+  console.log(second);
+}
+// Call the function and pass in the array.
+// This will only log the second topping as a string.
+logToppings(toppings);
