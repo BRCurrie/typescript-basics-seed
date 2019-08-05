0152121824bf805aec1926773365e6716eba8328
Ben Currie <benjaminrussellcurrie@gmail.com>
2019-08-05 14:15:10 -0500

    Null_Undefined_and_Strict_Null_Checks

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,25 @@
 // Run `tsc` then `node dist/app.js` to see result in console.
 
-// Never type is used when the function never actually returns something, or in our case
-// an error is thrown.
-function orderError(error: string): never {
-  throw new Error(error);
-  // We are never going to return a value!
+// tsconfig.json strict type checking was changed to false for this chapter.
+// this will allow us to set the value to null.
+let coupon: string = 'pizza25';
+// The pipe operator would allow the variable to be set as a string or a null value.
+let couponOrNull: string | null = 'pizza26';
+// remove data from coupon variable such as an incorrect entry needing to be reset.
+function removeCoupon(): void {
+  // changing strictNullCheck to true will identify an issue with setting a string value to null.
+  coupon = null;
 }
 
-orderError('Something went wrong');
+// log initial coupon value.
+
+console.log(coupon);
+
+// reset coupon by calling the function
+removeCoupon();
+
+// log nullified value.
+console.log(coupon);
+
+// Set strictNullCheck to true to see the changes.
+// Set strict typechecking to true to continue with the next chapters.
