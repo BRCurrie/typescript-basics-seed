c0014eaa7b7fd5f9c6fd4327c96c1d17352dfb0c
Ben Currie <benjaminrussellcurrie@gmail.com>
2019-08-06 11:15:31 -0500

    Functions_and_Optional_Arguments

--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,19 @@
 // Run `tsc` then `node dist/app.js` to see result in console.
 
-// Named Function
-function sumOrder(price: number, quantity: number): number {
-  return price * quantity;
-}
+// Adding the ? behind quantity makes it optional.
+let sumNumber: (price: number, quantity?: number) => number;
 
-// Setting up to recieve a function that was not yet defined.
-// Useful if we dont know what the signature of the function might be.
-let sumOrderLet: Function;
+// This is the original function.
+// sumNumber = (x, y) => x * y;
 
-// If using a function called by another library or that was not yet defined.
-// We can change the value of this function.
-sumOrderLet = (price: number, quantity: number): number => {
-  return price * quantity;
+// This conditional statement gives a default quantity equal to 1 if no argument is supplied.
+sumNumber = (x, y) => {
+  if (y) {
+    return x * y;
+  }
+  return x;
 };
 
-const sum = sumOrderLet(25, 2);
+const sumNum = sumNumber(15);
 
-console.log(`Total sum: ${sum}`);
-
-// Setting up a function which will return a type of number. Input variable names are needed.
-let sumNumber: (price: number, quantity: number) => number;
-
-// We can also change the variable names and use implicit return.
-// This will show us that x is price: number and y is quantity: number.
-sumNumber = (x, y) => x * y;
-
-const sumNum = sumNumber(15, 2);
-
-console.log(`Second total: ${sumNum}`);
+console.log(`Total: ${sumNum}`);
