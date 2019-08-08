b5eec675968a65e26805ccc1e23e216c1f1fc339
Ben Currie <benjaminrussellcurrie@gmail.com>
2019-08-08 12:34:37 -0500

    Object_Types

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,15 @@
 // Run `tsc` then `node dist/app.js` to see result in console.
 
-// Adding the ? behind quantity makes it optional.
-let sumNumber: (price: number, quantity?: number) => number;
+// The pizza below is given an object type with properties of name of type string, etc.
+// We can add a function property that will return a string.
+let pizza: { name: string; price: number; getname(): string };
 
-// This is the original function.
-// sumNumber = (x, y) => x * y;
+pizza = {
+  name: 'Plain Old Pepperoni',
+  price: 20,
+  getname() {
+    return pizza.name;
+  }
+};
 
-// This conditional statement gives a default quantity equal to 1 if no argument is supplied.
-// sumNumber = (x, y) => {
-//   if (y) {
-//     return x * y;
-//   }
-//   return x;
-// };
-
-// Alternate function for sumNumber above.
-// altNumber = (x, y) => {
-//     const q = y || 1;
-//     return x * q;
-// }
-
-// Really shortened code. Y now has a default quantity of 1 and can be replaced with
-// arguments passed in when the function is called.
-sumNumber = (x, y = 1) => x * y;
-
-const sumNum = sumNumber(15);
-
-console.log(`Total: ${sumNum}`);
+console.log(pizza.getname());
