91058d1bffe83defac314d344a42be5316d7a632
Ben Currie <benjaminrussellcurrie@gmail.com>
2019-08-08 12:28:12 -0500

    Typed_Functions_and_Default_Params

--- src/app.ts
+++ src/app.ts
@@ -7,12 +7,22 @@ let sumNumber: (price: number, quantity?: number) => number;
 // sumNumber = (x, y) => x * y;
 
 // This conditional statement gives a default quantity equal to 1 if no argument is supplied.
-sumNumber = (x, y) => {
-  if (y) {
-    return x * y;
-  }
-  return x;
-};
+// sumNumber = (x, y) => {
+//   if (y) {
+//     return x * y;
+//   }
+//   return x;
+// };
+
+// Alternate function for sumNumber above.
+// altNumber = (x, y) => {
+//     const q = y || 1;
+//     return x * q;
+// }
+
+// Really shortened code. Y now has a default quantity of 1 and can be replaced with
+// arguments passed in when the function is called.
+sumNumber = (x, y = 1) => x * y;
 
 const sumNum = sumNumber(15);
 
