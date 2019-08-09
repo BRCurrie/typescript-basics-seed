36adfdcf66c1abb363bc53a849422215de2ea023
Ben Currie <benjaminrussellcurrie@gmail.com>
2019-08-09 11:45:32 -0500

    Creating Interfaces

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,19 @@
-console.log('Ahoy TypeScript!');
+// Run `tsc` then `node dist/app.js` to see result in console.
+
+// Creating an interface is very similar to creating a type.
+// Notice the = is missing in the interface syntax.
+interface Pizza {
+  name: string;
+  sizes: string[];
+}
+
+let pizza: Pizza;
+
+function createPizza(name: string, sizes: string[]): Pizza {
+  return {
+    name,
+    sizes
+  };
+}
+
+pizza = createPizza('Pepperoni', ['small', 'medium']);
