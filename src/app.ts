bab9a11a16d1f480ad8d9e563f533f17969cf8dd
Ben Currie <benjaminrussellcurrie@gmail.com>
2019-08-08 20:34:01 -0500

    Array_Types_and_Generic

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,13 @@
 // Run `tsc` then `node dist/app.js` to see result in console.
 
-// The pizza below is given an object type with properties of name of type string, etc.
-// We can add a function property that will return a string.
-let pizza: { name: string; price: number; getname(): string };
+let sizes: string[];
 
-pizza = {
-  name: 'Plain Old Pepperoni',
-  price: 20,
-  getname() {
-    return pizza.name;
-  }
-};
+sizes = ['small', 'medium', 'large'];
 
-console.log(pizza.getname());
+// Generic types. Below it expects an array full of strings.
+let toppings: Array<string>;
+
+toppings = ['pepperoni', 'bacon', 'olives', 'pineapple'];
+
+// Contstructor style
+// new Array()
