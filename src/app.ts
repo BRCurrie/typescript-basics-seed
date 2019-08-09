c1a8c39c7524261f70f92684d3178a45773d8203
Ben Currie <benjaminrussellcurrie@gmail.com>
2019-08-08 20:40:02 -0500

    Tuple_Types_for_Arrays

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,9 @@
 // Run `tsc` then `node dist/app.js` to see result in console.
 
-let sizes: string[];
+// Tuples allow us to suggest to TS some kind of data structure inside of an array,
+// which is made up of different types.
 
-sizes = ['small', 'medium', 'large'];
-
-// Generic types. Below it expects an array full of strings.
-let toppings: Array<string>;
-
-toppings = ['pepperoni', 'bacon', 'olives', 'pineapple'];
-
-// Contstructor style
-// new Array()
+let pizza: [string, number, boolean];
+// You will only want to do this when you can be sure the data structure will match this exactly.
+pizza = ['Pepperoni', 20, true];
+// You cannot rearrange the data structure. It must match the tuple type.
