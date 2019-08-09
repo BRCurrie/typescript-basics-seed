2cc998b8874b8ad57243f3cd4cc2a60f8c618de6
Ben Currie <benjaminrussellcurrie@gmail.com>
2019-08-09 11:50:29 -0500

    Interfaces with Function Types

--- src/app.ts
+++ src/app.ts
@@ -2,17 +2,23 @@
 
 // Creating an interface is very similar to creating a type.
 // Notice the = is missing in the interface syntax.
+// Functions can be added to the interface then attached to objects and classes.
 interface Pizza {
   name: string;
   sizes: string[];
+  getAvailableSizes(): string[];
 }
 
 let pizza: Pizza;
 
+// We want to add a function to get available sizes.
 function createPizza(name: string, sizes: string[]): Pizza {
   return {
     name,
-    sizes
+    sizes,
+    getAvailableSizes() {
+      return this.sizes;
+    }
   };
 }
 
