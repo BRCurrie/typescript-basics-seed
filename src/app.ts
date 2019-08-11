15f00d3903de6fdf5460f7b4ed4fea2c06aeee9a
Ben Currie <benjaminrussellcurrie@gmail.com>
2019-08-10 23:54:13 -0500

    Setters and Getters

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,41 @@
 // 'tsc' in terminal, then 'node dist/app.js' to run in terminal.
 
-// Readonly modifier only allows us to read from the value.
-// Obviously we cannot write to this property.
+// Setting the value of a property.
+// element.className = 'abc'
+// Getting the property.
+// console.log(element.className);
+
+class Sizes {
+  constructor(public sizes: string[]) {}
+
+  // Properties here do not work the same way as the constructor();
+  set availableSizes(sizes: string[]) {
+    this.sizes = sizes;
+  }
+
+  // Similarly we could set the above function as a date string with a date inside.
+  // Then before we assing the date we could call the new date constructor and then
+  // define that in the class.
+
+  get availableSizes() {
+    return this.sizes;
+  }
+}
+
+// The setter allows us to do this. We create a new instance of the class and
+// pass in the string array.
+const sizes = new Sizes(['small', 'medium']);
+// Invoke getter function.
+console.log(sizes.availableSizes);
+// Set the available sizes to something else as if it was for another type of pizza.
+// Override the value initially set.
+sizes.availableSizes = ['medium', 'large'];
+// Invoke getter function again, this time with updated array.
+console.log(sizes.availableSizes);
 
 class Pizza {
   toppings: string[] = [];
 
-  // The property will be available outside of the class but cannot be changed.
   constructor(readonly name: string) {}
 
   addTopping(topping: string) {
@@ -18,8 +47,4 @@ const pizza = new Pizza('Pepperoni');
 
 pizza.addTopping('pepperoni');
 
-// Logs Pepperoni from the above const. It is being read at this time.
 console.log(pizza.name);
-
-// Does not work.
-pizza.name = 'ABC';
