6981c7dc019c0854ea9590a593a4dbb1dca61f67
Ben Currie <benjaminrussellcurrie@gmail.com>
2019-08-12 12:17:47 -0500

    Class Inheritance

--- src/app.ts
+++ src/app.ts
@@ -8,43 +8,35 @@
 class Sizes {
   constructor(public sizes: string[]) {}
 
-  // Properties here do not work the same way as the constructor();
   set availableSizes(sizes: string[]) {
     this.sizes = sizes;
   }
-
-  // Similarly we could set the above function as a date string with a date inside.
-  // Then before we assing the date we could call the new date constructor and then
-  // define that in the class.
 
   get availableSizes() {
     return this.sizes;
   }
 }
 
-// The setter allows us to do this. We create a new instance of the class and
-// pass in the string array.
-const sizes = new Sizes(['small', 'medium']);
-// Invoke getter function.
-console.log(sizes.availableSizes);
-// Set the available sizes to something else as if it was for another type of pizza.
-// Override the value initially set.
-sizes.availableSizes = ['medium', 'large'];
-// Invoke getter function again, this time with updated array.
-console.log(sizes.availableSizes);
-
-class Pizza {
+// We extend the Pizza class with the Sizes class so we can access sizes for each pizza.
+class Pizza extends Sizes {
   toppings: string[] = [];
-
-  constructor(readonly name: string) {}
+  // Constructors for derived classes must use a super call.
+  // We add the sizes: string[] to the argument, and pass sizes into the super call.
+  constructor(readonly name: string, public sizes: string[]) {
+    // We do not just pass in the array here, we pass it in when we create each pizza.
+    super(sizes);
+  }
 
   addTopping(topping: string) {
     this.toppings.push(topping);
   }
 }
-
-const pizza = new Pizza('Pepperoni');
+// We pass in the available sizes for the super call here.
+const pizza = new Pizza('Pepperoni', ['small', 'medium']);
 
 pizza.addTopping('pepperoni');
 
 console.log(pizza.name);
+// We can now use the inherited getters and setters from the sizes class.
+// We retrieve the sizes now.
+console.log(pizza.availableSizes);
