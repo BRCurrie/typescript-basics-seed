a5d0af00ec51a73458cde61acd42d42507da7684
Ben Currie <benjaminrussellcurrie@gmail.com>
2019-08-12 21:48:55 -0500

    Static Properties and Methods

--- src/app.ts
+++ src/app.ts
@@ -1,56 +1,35 @@
 // 'tsc' in terminal, then 'node dist/app.js' to run in terminal.
 
-// We do not currently have any interfaces to work with our classes.
-// You can only define properties. You cannot specify if it is for a setter or getter.
-// This is where unit tests come into play.
-interface SizesInterface {
-  // We cannot define private/protected properties in the interface.
-  // It would work if it was public.
-  // sizes: string[];
-  availableSizes: string[];
-}
+// The whole point of static is to access properties or methods without having to
+// create a new instance.
 
-// We add the implements keyword and our interface.
-abstract class Sizes implements SizesInterface {
-  constructor(protected sizes: string[]) {}
+// We do not need to do anything major.
+// Comes in very handy when we need to create utility libraries, functions,
+// or classes that do particular things, like pure functions that don't mutate
+// or deal with datasets.
 
-  set availableSizes(sizes: string[]) {
-    this.sizes = sizes;
-  }
+// Typical example of a static property.
+// const date = Date.now();
+// console.log(date);
 
-  get availableSizes() {
-    return this.sizes;
-  }
-}
+// Create static method or property with a class.
+class Coupon {
+  // This is a static property.
+  static allowed = ['Pepperoni', 'Blazing Inferno'];
 
-// Just like the pizza class extends the Sizes interface,
-// the PizzaInterface extends the SizesInterface.
-// This is all done for type checking purposes. It would show a red underline
-// in the editor if something was mispelled for example.
-interface PizzaInterface extends SizesInterface {
-  readonly name: string;
-  toppings: string[];
-  // Both functions will not return anything, so the type is void.
-  updateSizes(sizes: string[]): void;
-  addTopping(topping: string): void;
-}
-
-class Pizza extends Sizes implements PizzaInterface {
-  toppings: string[] = [];
-  constructor(readonly name: string, sizes: string[]) {
-    super(sizes);
-  }
-  public updateSizes(sizes: string[]) {
-    this.sizes = sizes;
-  }
-
-  addTopping(topping: string) {
-    this.toppings.push(topping);
+  //   Static Method. If we want to prefix our coupon code then add the discount %.
+  static create(percentage: number) {
+    return `PIZZA_RESTAURANT_${percentage}`;
   }
 }
-const pizza = new Pizza('Pepperoni', ['small', 'medium']);
 
-pizza.addTopping('pepperoni');
-console.log(pizza.availableSizes);
-pizza.updateSizes(['large']);
-console.log(pizza.availableSizes);
+// Because the code for allowed is a purely static property,
+// we do not need to create a new instance.
+// new Coupon9();
+
+// We can just access this property directly.
+console.log(Coupon.allowed);
+
+// Does not properly indicate a percentage value, but demonstrates how it would
+// be used as a static method.
+console.log(Coupon.create(25));
